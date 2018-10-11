e4c511bfe375819d94b990b0bcee237ed4018e43
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-10-11 13:39:13 +0200

    Some User tests

--- CoreTest/UserServiceTest.cs
+++ CoreTest/UserServiceTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Moq;
 using WebShop.Core;
+using WebShop.Core.ApplicationService;
+using WebShop.Core.ApplicationService.Impl;
+using WebShop.Core.DomainService;
 using WebShop.Core.Entity;
 using Xunit;
 
@@ -12,7 +15,42 @@ namespace CoreTest
 
         [Fact]
         public void LoginWithValidUsernameAndPassword(){
+            //Setup
+            Mock<IUserRepository> mockRepo = new Mock<IUserRepository>();
 
+            var testStringForHash = "qwertyuiopasdfghjk1234567890";
+            var testUsername = "MyName";
+
+            mockRepo.Setup(x => x.GetUserByUsername(It.IsAny<string>())).Returns(() => new User() { PasswordHash = testStringForHash });
+
+            IUserService userService = new UserService(mockRepo.Object);
+
+            //Test
+
+            var returndUser = userService.Login(new User() { Username = testUsername, PasswordHash = testStringForHash });
+
+            Assert.NotNull(returndUser);
+        }
+
+        [Fact]
+        public void LoginWithNonValidPassword(){
+            //Setup
+            Mock<IUserRepository> mockRepo = new Mock<IUserRepository>();
+
+            var testStringForHash = "qwertyuiopasdfghjk1234567890";
+            var closeButNotQuite = "qwertyuiopasdfghjk123456789";
+            var testUsername = "MyName";
+
+            mockRepo.Setup(x => x.GetUserByUsername(It.IsAny<string>())).Returns(() => new User() { PasswordHash = testStringForHash });
+
+            IUserService userService = new UserService(mockRepo.Object);
+
+            //Test
+            User returndUser = null;
+
+            Assert.Throws<ArgumentException>(() => returndUser = userService.Login(new User() { Username = testUsername, PasswordHash = closeButNotQuite }));
+
+            Assert.Null(returndUser);
         }
 
 
